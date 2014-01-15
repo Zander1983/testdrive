da4f63bc0cfd6f382e6cc0585e84818f228d25ba
web-intelligence <info@webintelligence.ie>
2014-01-15 17:11:35 +0000

    third commit

--- protected/models/Article.php
+++ protected/models/Article.php
@@ -162,15 +162,24 @@ class Article extends CActiveRecord
             $tickerText   = "ticker text message";
             $contentTitle = "content title";
             $contentText  = "content body";
+            
+            /*
+            message.addData('message',"\u270C Peace, Love \u2764 and PhoneGap \u2706!");
+            message.addData('title','Push Notification Sample' );
+            message.addData('msgcnt','3'); // Shows up in the notification in the status bar
+            message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+            //message.collapseKey = 'demo';
+            //message.delayWhileIdle = true; //Default is false
+            message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+            */
 
             $response = $this->sendNotification( 
                             $project->api_key, 
                             $registrationId, 
                             array(
-                                'message' => $message, 
-                                'tickerText' => $tickerText, 
-                                'contentTitle' => $contentTitle, 
-                                "contentText" => $contentText) );
+                                'title' => $this->title,
+                                'message' => $this->article_url, 
+                                'msgcnt' => '3'));
 
             $response = json_decode($response);
 
